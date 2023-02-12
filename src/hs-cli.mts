a0e92b70649714a6bcd236e18be195f94d5a1875
Jason Li <jasonmli@outlook.com>
2023-02-12 22:38:37 +0800

    Initial commit

---
+++ src/hs-cli.mts
@@ -0,0 +1,8 @@
+import { readFileSync, writeFileSync } from 'fs';
+import { hc_compile, hso_to_html_file } from './hs-core.js'
+
+if (process.argv.length <= 2)
+  throw Error("Please input filename in command line.");
+const sourceFile = readFileSync(process.argv[2]).toString();
+const htmlFile = hso_to_html_file(hc_compile(sourceFile));
+writeFileSync(process.argv[2].split(".")[0] + ".html", htmlFile, { encoding: 'utf-8' });
---
+++ src/hs-xml.d.ts
@@ -0,0 +1,100 @@
+import type { tNode } from "txml/txml"
+
+interface XmlSelfClosingElement extends tNode {
+  tagName: string,
+  attributes: Record<string, string | undefined>,
+  children: [],
+}
+
+interface XmlGeneralElement extends tNode {
+  tagName: string,
+  attributes: Record<string, string | undefined>,
+  children: XmlElement[],
+}
+
+type XmlElement = string | XmlSelfClosingElement | XmlGeneralElement;
+
+interface XmlTerminalElement extends XmlGeneralElement {
+  children: [string],
+}
+
+interface XmlMetaModElement extends XmlTerminalElement {
+  tagName: 'Mod',
+  attributes: { color: string },
+}
+
+interface XmlMetaModsElement extends XmlGeneralElement {
+  tagName: 'Mods',
+  children: XmlMetaModElement[],
+}
+
+interface XmlMetaTerminalElement extends XmlTerminalElement {
+  tagName: 'Lang' | 'Author' | 'Id' | 'Date' | 'Created',
+}
+
+export type XmlMetaElement = XmlMetaTerminalElement | XmlMetaModsElement;
+
+interface XmlInlineBaseElement extends XmlGeneralElement {
+  children: XmlInlineElement[],
+}
+
+interface XmlInlineBoldElement extends XmlInlineBaseElement {
+  tagName: 'b',
+}
+
+interface XmlInlineItalicElement extends XmlInlineBaseElement {
+  tagName: 'i',
+}
+
+interface XmlInlineDeleteElement extends XmlInlineBaseElement {
+  tagName: 'del',
+}
+
+interface XmlInlineBackgroundElement extends XmlInlineBaseElement {
+  tagName: 'bg',
+  attributes: { color: string, v: undefined } | { v: string, color: undefined },
+}
+
+interface XmlInlineAnchorElement extends XmlInlineBaseElement {
+  tagName: 'a',
+  attributes: { href: string },
+}
+
+interface XmlInlineImageElement extends XmlSelfClosingElement {
+  tagName: 'img',
+  attributes: { src: string, alt: string, w?: string, h?: string },
+}
+
+interface XmlInlineNoteElement extends XmlSelfClosingElement {
+  tagName: 'note',
+  attributes: { v: string },
+}
+
+export type XmlInlineElement = string | XmlInlineAnchorElement | XmlInlineBackgroundElement | XmlInlineBoldElement | XmlInlineDeleteElement | XmlInlineImageElement | XmlInlineImageElement | XmlInlineItalicElement | XmlInlineNoteElement;
+
+interface XmlItemElement extends XmlGeneralElement {
+  tagName: 'Item',
+  attributes: {
+    id: string,
+    deleted?: string,
+    new?: string,
+  },
+  children: XmlInlineElement[],
+}
+
+export interface XmlSubjectElement extends XmlGeneralElement {
+  tagName: 'Subject',
+  attributes: { name: string },
+  children: XmlItemElement[],
+}
+
+export interface XmlNoteElement extends XmlGeneralElement {
+  tagName: 'Note',
+  children: XmlInlineElement[],
+}
+
+export type XmlHomeworkElements = [
+  { children: XmlMetaElement[] },
+  { children: XmlSubjectElement[] },
+  { children: XmlNoteElement[] },
+]
